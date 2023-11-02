225836ddee347f1159b83460fc33f4a647102124
Niranjan B K <bkniranjan19975@gmail.com>
2023-11-02 09:42:48 +0530

    Add multiple addresses functionality to the form, Add ng-material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +9,21 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   signupForm!: FormGroup;
 
+  emailRequiredError = false;
+  emailFormatError = false;
+  passwordRequiredError = false;
+  passwordMinLengthError = false;
+  confirmPasswordRequiredError = false;
+  notSameError = false;
+
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.signupForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', Validators.required]
+      confirmPassword: ['', Validators.required],
+      addresses: this.formBuilder.array([this.createAddress()])
     }, {validator: this.checkPasswords });
   }
 
@@ -25,10 +33,46 @@ export class AppComponent implements OnInit {
     return password === confirmPassword ? null : { notSame: true };
   }
 
+  setErrors() {
+    const emailCtrl = this.signupForm.get('email');
+    this.emailRequiredError = emailCtrl!.hasError('required');
+    this.emailFormatError = emailCtrl!.hasError('email');
+
+    const passwordCtrl = this.signupForm.get('password');
+    this.passwordRequiredError = this.hasError('password','required');
+    this.passwordMinLengthError = this.hasError('password','minlength');
+
+    const confirmPasswordCtrl = this.signupForm.get('confirmPassword');
+    this.confirmPasswordRequiredError = confirmPasswordCtrl!.hasError('required');
+
+    this.notSameError = this.signupForm.hasError('notSame');
+  }
+
   hasError(controlName: string, errorName: string): boolean {
     const control = this.signupForm.get(controlName);
     return control && control.errors && control.errors[errorName];
   }
+
+  createAddress(): FormGroup {
+    return this.formBuilder.group({
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      zip: ['', Validators.required]
+    });
+  }
+
+  get addresses(): FormArray {
+    return this.signupForm.get('addresses') as FormArray;
+  }
+
+  addAddress(): void {
+    this.addresses.push(this.createAddress());
+  }
+
+  removeAddress(index: number): void {
+    this.addresses.removeAt(index);
+  }
+
 
   onSubmit() {
     if (this.signupForm.valid) {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 
 @NgModule({
   declarations: [
@@ -11,8 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
